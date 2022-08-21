593532129d279d46ca82c5a792ecf7583ea1859f
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2022-08-21 16:47:07 +0900

    dont use thumbnail

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import YouTube from 'react-youtube'
 import { useVideo, UseVideoOptions } from './useVideo'
-import { fetchThumbnailUrl } from '../../utility/fetchThumbnail'
 
 type VideoProps = UseVideoOptions
 
@@ -10,21 +9,9 @@ const Video: React.FC<VideoProps> = ({ playAt, playNextVideo }) => {
     playAt,
     playNextVideo,
   })
-  const [img, setImg] = useState('')
-  useEffect(() => {
-    fetchThumbnailUrl(videoId).then((x) => setImg(x || ''))
-  }, [videoId])
-  console.log(img)
 
   return (
     <div className="relative">
-      <img
-        className="blur-lg object-cover static"
-        src={img}
-        alt={'thumbnail'}
-        width={1080}
-        height={720}
-      />
       <YouTube
         className="flex justify-center h-full absolute top-10 left-10"
         videoId={videoId}

4eac395c4d1417a590cdbd6e60717f977dd2e1a0
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2022-08-22 12:36:59 +0900

    add recoil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../components/config'
+import dynamic from 'next/dynamic'
+import { RecoilRoot } from 'recoil'
+
+const YoutubePlayer = dynamic(() => import('@/layers/YoutubePlayer'), {
+  ssr: false,
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <RecoilRoot>
       <Header />
+      <YoutubePlayer />
       <Component {...pageProps} />
-    </>
+    </RecoilRoot>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import type { NextPage } from 'next'
-import { PlayList } from '../components/PlayList'
+import { useEffect } from 'react'
+import { getVideoList } from '@/utility/localStorage'
+import { useSetRecoilState, useRecoilValue } from 'recoil'
+import { videoTermState, videoReadyEventState } from '@/atoms/player'
 
 const Index: NextPage = () => {
+  console.log('Index')
+  const setVideoTerm = useSetRecoilState(videoTermState)
+  const videoEvent = useRecoilValue(videoReadyEventState)
+
+  useEffect(() => {
+    const { videoId, start, end } = getVideoList()[0]
+    setVideoTerm({ videoId, start, end })
+  }, [setVideoTerm])
+
   return (
     <div className="w-screen h-screen bg-peanuts1">
       <h1>Youtube</h1>
-      <PlayList />
+      <button onClick={() => videoEvent.playVideo()}>play</button>
+      <button onClick={() => videoEvent.pauseVideo()}>stop</button>
     </div>
   )
 }
